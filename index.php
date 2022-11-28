349923c49a933c18ba1ce350b4cc4579783dfec3
mauriciocastaeda <117041636+mauriciocastaeda@users.noreply.github.com>
2022-11-27 18:44:17 -0600

    Create index.php
    index

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$servidor = 'localhost:43065';
+$cuenta = 'root';
+$password = '';
+$bd = 'bdgrafica';
+
+$conexion = new mysqli($servidor, $cuenta, $password, $bd);
+
+$sql = "SELECT NombreProd, Cantidad FROM ventastotales";
+$res = $conexion->query($sql);
+?>
+<html>
+
+<head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Productos', 'Cantidad de unidades vendidas'],
+                <?php
+                while($fila = $res->fetch_assoc()){
+                    echo "['".$fila["NombreProd"]."',".$fila["Cantidad"]."],";
+                }
+                ?>
+            ]);
+
+            var options = {
+                title: 'Gráfica de ventas totales hasta la fecha'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+
+<body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+</body>
+
+</html>
