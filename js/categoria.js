5b997e38874022fd4eca0ae5b5d9036108b7b7af
Robusto <50712323+Prograrock1292@users.noreply.github.com>
2022-12-09 23:55:37 -0600

    afsasfsa

--- js/categoria.js
+++ js/categoria.js
@@ -1,9 +1,20 @@
 function filtrar() {
-    const filtro = new XMLHttpRequest();
+    /*const filtro = new XMLHttpRequest();
     var filtroCat = ("input[type=radio][name=radioCat]:checked").val;
     console.log(filtroCat);
     filtro.onload = function() {
-        /*var filtroCat = $("input[type=radio][name=radioCat]:checked").val;
-        console.log(filtroCat);*/
-    }
+        var filtroCat = $("input[type=radio][name=radioCat]:checked").val;
+        console.log(filtroCat);
+    }*/
+    $(document).ready(function() {
+        $('button').click(function() {
+            var value = $("input[type=radio][name=contact]:checked").val();
+            if (value) {
+                alert(value);
+            }
+            else {
+                alert('Nothing is selected');
+            }
+        })
+    });
 }
