002f482306dcc9f7b236e1bb5129b8a7cd334637
Robusto <50712323+Prograrock1292@users.noreply.github.com>
2022-12-11 14:58:42 -0600

    favicon

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ if (isset($_POST['logout'])) {
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/daf8eb91e6.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="images/BoomBoxLogo4Img.png">
     <style>
         .offcanvas-backdrop {
             background-color: rgba(0, 0, 0, .5) !important;
